ab9f543114d405d53789d95cdf8c9bb2908b012d
vararais <varadigmaisnann@gmail.com>
2024-05-11 11:51:54 +0700

    Menambah Void Pop

--- Stack/Stack.cpp
+++ Stack/Stack.cpp
@@ -27,6 +27,12 @@ public:
         cout << "Push value: " << value << endl;
         return value;
     }
+
+    void pop() {
+        if (isEmpty()) {
+            cout << "Stack is empty." << endl;
+        }
+    }
 };
 int main()
 {
